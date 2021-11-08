a957cc299c3587d89c97b5c882a18a04bac3d1cf
Exequiel <exequielreyes93@gmail.com>
2021-11-08 13:11:17 -0300

    algunos cambios
    carrito

---
+++ src/Componentes/ProdCarrito.jsx
@@ -0,0 +1,77 @@
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import img1 from "../img/volante-corona.jpg";
+import { makeStyles } from '@mui/styles';
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import BotonPrimario from "./BotonPrimario";
+import BotonSecundario from "./BotonSecundario";
+
+const useStyle = makeStyles({
+  img: {
+    height: "75%",
+    alt:"",
+     //Falta poner el object fit cover
+  }
+})
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: "none",
+    textAlign: "center",
+    height: "100%",
+    color: theme.palette.text.secondary,
+  }));
+
+export default function ProdCarrito(props) {
+
+    const classes = useStyle();
+
+  return (
+    <Grid container spacing={0}  >
+        {/* <Grid  justify="center" > */}
+             <Grid item xs={2} height="195px" 
+             style={{boxShadow:"none !important",}}
+             >
+                <Item>
+                  <img src={img1} className={classes.img}/>
+                </Item>
+            {/* </Grid> */}
+        </Grid>
+     
+      <Grid item xs={5}>
+      <Item>
+            <Box component="div" pl="10px" pt="2%" pr="200px">
+                <Typography  style={{fontSize:"25px"}}variant="h4" color="black" textAlign="left">Volante motor con corona</Typography>
+                <Typography  style={{fontSize:"18px", fontWeight:"bold"}} variant="subtitle2" color="black" textAlign="left" pl="3px" mt="5%">Descripcion</Typography>
+                <Typography variant="body1" color="black"  pl="3px"  textAlign="left"> M.Benz 1114 Motor OM-352 Placa Ø 280 m.m.(Corona Ancha) Con corona </Typography>
+                
+                {/* <ul>
+                    <li>Eliminar</li>
+                    <li><a>Comrar ahora</a></li>
+                </ul> */}
+      
+            </Box>
+        </Item>
+            
+    
+
+      </Grid>   
+       
+
+
+      <Grid item xs={3} height="195px">
+                <Item>
+                  <img src={img1} className={classes.img}/>
+                </Item>
+            {/* </Grid> */}
+        </Grid>
+
+
+
+         
+      </Grid>
+    
+  );
+}
---
+++ src/Paginas/Carrito.jsx
@@ -0,0 +1,84 @@
+
+import NavBar from "../Componentes/AppBar";
+import Categorias from "../Componentes/Categorias";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Footer from "../Componentes/Footer";
+import img1 from "../img/nosotros1.png";
+import img2 from "../img/nosotros2.png";
+import img3 from "../img/nosotros3.png";
+import { Typography } from "@mui/material";
+import ProdCarrito from "../Componentes/ProdCarrito";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: "none",
+  textAlign: "center",
+  height: "100%",
+  color: theme.palette.text.secondary,
+}));
+
+const Carrito = () => {
+  return (
+    <>
+      <NavBar />
+      <Categorias />
+      <Box sx={{ flexGrow: 1 }}>
+
+      <Grid container spacing={1}>
+        <Grid item xs={12}  >
+          <Item> 
+            <ProdCarrito/>
+          </Item>
+        </Grid>
+        {/* <Grid item xs={3}  >
+          <Item>xs=3</Item>
+        </Grid>
+        <Grid item xs={3}  >
+          <Item>xs=3</Item>
+        </Grid>
+        <Grid item xs={3}  >
+          <Item>xs=3</Item>
+        </Grid>
+        */}
+      </Grid> 
+    </Box>
+  
+
+{/*        
+        <Grid container>
+          <Grid item xs={6}>
+           
+          </Grid>
+          <Grid item xs={6}>
+            
+          </Grid>
+          <Grid item xs={6}>
+           
+          </Grid>
+          <Grid item xs={6}>
+             
+          </Grid>
+          <Grid item xs={6}>
+             
+          </Grid>
+          <Grid item xs={6}>
+           
+          </Grid>
+        
+   
+       
+          </Grid>
+*/}
+       
+      <Footer />
+    </>
+  );
+}
+
+
+export default Carrito;
+
+
