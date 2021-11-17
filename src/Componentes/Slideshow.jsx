f7b9f291050df9c376da031aeaeeb1bb2c24cf97
Leo Moreno <leonel_moreno99@hotmail.com>
2021-11-17 17:41:46 -0300

    Correccion error del carrusel

--- src/Componentes/Slideshow.jsx
+++ src/Componentes/Slideshow.jsx
@@ -8,41 +8,46 @@ const Slideshow = ({
 		controles = false,
 		autoplay = false,
 		velocidad="500",
-		intervalo="5000"
+		intervalo="3000"
 	}) => {
 	const slideshow = useRef(null);
 	const intervaloSlideshow = useRef(null);
 
 	const siguiente = useCallback(() => {
 		// Comprobamos que el slideshow tenga elementos
-		if(slideshow.current.children.length > 0){
-			// console.log('Siguiente')
-
-			// Obtenemos el primer elemento del slideshow.
-			const primerElemento = slideshow.current.children[0];
-
-			// Establecemos la transicion para el slideshow.
-			slideshow.current.style.transition = `${velocidad}ms ease-out all`;
-
-			const tamañoSlide = slideshow.current.children[0].offsetWidth;
-
-			// Movemos el slideshow
-			slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
-
-			const transicion = () => {
-				// Reiniciamos la posicion del Slideshow.
-				slideshow.current.style.transition = 'none';
-				slideshow.current.style.transform = `translateX(0)`;
-
-				// Tomamos el primer elemento y lo mandamos al final.
-				slideshow.current.appendChild(primerElemento);
-
-				slideshow.current.removeEventListener('transitionend', transicion);
+		console.log(slideshow)
+		if(slideshow.current != null){
+			if(slideshow.current.children.length > 0){
+				// console.log('Siguiente')
+	
+				// Obtenemos el primer elemento del slideshow.
+				const primerElemento = slideshow.current.children[0];
+	
+				// Establecemos la transicion para el slideshow.
+				slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+	
+				const tamañoSlide = slideshow.current.children[0].offsetWidth;
+	
+				// Movemos el slideshow
+				slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+	
+				const transicion = () => {
+					// Reiniciamos la posicion del Slideshow.
+					slideshow.current.style.transition = 'none';
+					slideshow.current.style.transform = `translateX(0)`;
+	
+					// Tomamos el primer elemento y lo mandamos al final.
+					slideshow.current.appendChild(primerElemento);
+	
+					slideshow.current.removeEventListener('transitionend', transicion);
+				}
+	
+				// Eventlistener para cuando termina la animacion.
+				slideshow.current.addEventListener('transitionend', transicion);
+	
 			}
-
-			// Eventlistener para cuando termina la animacion.
-			slideshow.current.addEventListener('transitionend', transicion);
-
+		}else{
+			console.log("slideshow nulo")
 		}
 	}, [velocidad]);
 	
